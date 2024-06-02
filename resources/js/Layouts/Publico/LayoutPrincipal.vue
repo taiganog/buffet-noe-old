30aa906688e49a278fbcbcf745c45094aba71fd7
taiga <taiga.nog.b@gmail.com>
2024-06-02 12:59:41 -0300

    Aumentando responsividade no layout base e tela principal

--- resources/js/Layouts/Publico/LayoutPrincipal.vue
+++ resources/js/Layouts/Publico/LayoutPrincipal.vue
@@ -2,10 +2,10 @@
 	
 </script>
 <template>
-	<header class="z-20 shadow shadow-black bg-neutral-900 sticky top-0">
-		<div class="flex align-center justify-between">
-			<a :href="route('principal')"><img src="img/publico/logo.png" class="w-50 h-20 mt-3 ml-20"></a>
-			<ul class="flex gap-10 text-zinc-200 text-xl p-8 mr-20 content-center text-center">
+	<header class="z-20 shadow shadow-black bg-neutral-900 md:sticky top-0">
+		<div class="md:flex grid md:align-center md:justify-between">
+			<a :href="route('principal')"><img src="img/publico/logo.png" class="w-50 h-20 mt-3 md:ml-20 mx-auto"></a>
+			<ul class="flex gap-10 text-zinc-200 text-xl p-8 md:mr-20 justify-center text-center">
 				<li class="duration-200 hover:text-2xl"><a :href="route('sobre_nos')">Sobre nós</a></li>
 				<li class="duration-200 hover:text-2xl"><a :href="route('fotos')">Fotos</a></li>
 				<li class="duration-200 hover:text-2xl"><a :href="route('contato')">Contato</a></li>
@@ -18,27 +18,25 @@
 	</section>
 
 	<footer >
-		<div class="py-1/5 lg:grid lg:grid-cols-8 sm:flex bg-neutral-900 text-zinc-200">
+		<div class="py-1/5 xl:grid xl:grid-cols-8 md:flex bg-neutral-900 text-zinc-200">
 			<!-- Icones -->
-			<div class="p-3 col-start-2">
-				<ul class="flex gap-10 mt-3">
-					<li><a href="https://www.instagram.com/buffet_arca_de_noe/" target="_blank"><img src="img/publico/instagram.png" class="h-10 w-10"></a></li>
-					<li><a href="https://wa.me/556194186180" target="_blank"><img src="img/publico/whatsapp.png" class="h-10 w-10"></a></li>
-					<li><a href="https://www.facebook.com/noebersoncruz/" target="_blank"><img src="img/publico/facebook.png" class="h-10 w-10"></a></li>
+			<div class="p-3 xl:col-start-2">
+				<ul class="flex gap-10 mt-3 justify-center">
+					<li><a href="https://www.instagram.com/buffet_arca_de_noe/" target="_blank"><img src="img/publico/instagram.png" class="h-10 w-10 object-scale-down"></a></li>
+					<li><a href="https://wa.me/556194186180" target="_blank"><img src="img/publico/whatsapp.png" class="h-10 w-10 object-scale-down"></a></li>
+					<li><a href="https://www.facebook.com/noebersoncruz/" target="_blank"><img src="img/publico/facebook.png" class="h-10 w-10 object-scale-down"></a></li>
 				</ul>
 			</div>
 			<!-- Endereço -->
-			<div class="text-center pt-5 col-start-4 col-span-2">
-				Suite 810 613 Friesen Mills,<br> Sauerchester, AR 68135-7288
-			</div>
+			<div class="text-center pt-5 xl:col-start-4 xl:col-span-2 p-5">Qd.02 16 - Mansões Village<br>Águas Lindas de Goiás - GO, 72916-215</div>
 			<!-- Logo -->
-			<div class="col-start-7">
-				<img src="img/publico/logo.png" class="w-50 h-20 mt-3">
+			<div class="xl:col-start-7">
+				<img src="img/publico/logo.png" class="w-50 h-20 block mt-3 mx-auto object-scale-down">
 			</div>
 			<!-- Creditos -->
-			<div class="col-start-1 col-end-9">
+			<div class="xl:col-start-1 xl:col-end-9">
 				<hr class="w-4/5 h-1 mx-auto bg-gray-100 border-0 rounded dark:bg-gray-700	">
-				<div class="text-zinc-200 text-center text-xs mt-3 mb-5">Desenvolvido e mantido por <i>Taiga Nogueira</i> - 2024/05</div>
+				<div class="text-zinc-200 text-center text-xs px-4 py-4">Desenvolvido e mantido por <i>Taiga Nogueira</i> - 2024/05</div>
 			</div>
 		</div>
 	</footer>
--- resources/js/Pages/Publico/Principal.vue
+++ resources/js/Pages/Publico/Principal.vue
@@ -6,11 +6,11 @@
 	<LayoutPrincipal>
 		<div class="flex">
 			<img src="img/publico/main-img.jpg" class="w-full brightness-75 object-cover overflow-hidden h-screen min-h-full border-b-black">
-			<div class="z-10 bg-black opacity-85 absolute text-zinc-200 left-44 top-44 rounded-xl max-w-4xl">
+			<div class="z-10 bg-black opacity-85 absolute text-zinc-200 xl:left-44 left-5 md:top-44 top-64 right-5 rounded-xl xl:max-w-4xl">
 				<div class="p-10">
 					<h2 class="text-center text-6xl font-black"><b>Arca de Noé</b></h2><br>
 					<h1 class="text-center text-3xl mb-5">Serviço de Buffet</h1><br>
-					<p class="text-justify text-xl">Descubra o sabor da celebração com nosso buffet excepcionalmente elaborado, onde cada detalhe é uma experiência sensorial única.</p>
+					<p class="text-justify text-xl">Descubra o sabor da celebração com nosso buffet excepcionalmente elaborado, onde cada detalhe é uma experiência sensorial única. Atendemos as suas necessidades em todo o DF e entorno.</p>
 				</div>
 			</div>
 		</div>
